b986ce6fe5824af36e8797177cdbeb458107710f
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-12 00:42:20 +0300

    add local.conf

--- test/timing_SUITE.erl
+++ test/timing_SUITE.erl
@@ -31,7 +31,17 @@ end_per_suite(_Config) ->
     ok.
 
 bunch1(Config) ->
+    Local = get_local_config(Config),
     Dat = stub,
     ct:pal("dat: ~p", [Dat]),
     ok.
+
+get_local_config(Config) ->
+    File = "local.conf",
+    Dir = ?config(data_dir, Config),
+    Path = filename:join([Dir, File]),
+    %% ct:pal("path: ~p", [Path]),
+    {ok, [Local]} = file:consult(Path),
+    %% ct:pal("local: ~p", [Local]),
+    Local.
 
